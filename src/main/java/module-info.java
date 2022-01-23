3e454fe19596bc96057cf2667d1ebdf50b8bf5c9
Sehkah Vhemrada <sehkah.vhemrada@gmail.com>
2022-01-23 01:19:08 +0100

    Fix module management for jackson & log4j.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,16 +6,13 @@ module org.sehkah.ddonbruteforcergui {
     requires org.kordamp.ikonli.javafx;
     requires org.bouncycastle.provider;
     requires com.sun.jna;
-    requires javafx.web;
-    requires java.logging;
     requires org.apache.logging.log4j;
-    requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.dataformat.yaml;
+    requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
 
     opens org.sehkah.ddonbruteforcergui.view.main to javafx.fxml;
     exports org.sehkah.ddonbruteforcergui to javafx.graphics;
     exports org.sehkah.ddonbruteforcergui.view.main to javafx.fxml, com.fasterxml.jackson.databind;
     exports org.sehkah.ddonbruteforcergui.controller.main to javafx.fxml;
-
 }
