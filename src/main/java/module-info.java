f2335673a3448f8f7db977fbaf662539f609211a
Sehkah Vhemrada <sehkah.vhemrada@gmail.com>
2022-01-23 11:39:12 +0100

    Fix jpackage runtime issues.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,15 +4,20 @@ module org.sehkah.ddonbruteforcergui {
 
     requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.bootstrapicons;
+
     requires org.bouncycastle.provider;
     requires com.sun.jna;
-    requires org.apache.logging.log4j;
+
     requires com.fasterxml.jackson.dataformat.yaml;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+
+    requires org.apache.logging.log4j;
     requires org.apache.commons.io;
 
     opens org.sehkah.ddonbruteforcergui.view.main to javafx.fxml;
+
     exports org.sehkah.ddonbruteforcergui to javafx.graphics;
     exports org.sehkah.ddonbruteforcergui.view.main to javafx.fxml, com.fasterxml.jackson.databind;
     exports org.sehkah.ddonbruteforcergui.controller.main to javafx.fxml;
