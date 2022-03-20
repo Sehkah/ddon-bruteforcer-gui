1aaf7b271e7d33cbd0f0b2b933c47c1bbc46e92e
Sehkah Vhemrada <sehkah.vhemrada@gmail.com>
2022-03-20 10:33:10 +0100

    Migrate serializer to Jackson's new JSON-specific mapper.

--- src/main/java/org/sehkah/ddonbruteforcergui/model/serialization/SerializerImpl.java
+++ src/main/java/org/sehkah/ddonbruteforcergui/model/serialization/SerializerImpl.java
@@ -1,17 +1,19 @@
 package org.sehkah.ddonbruteforcergui.model.serialization;
 
-import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 public class SerializerImpl implements Serializer {
-    private final ObjectMapper mapper;
+    private final JsonMapper mapper;
 
     public SerializerImpl() {
-        mapper = new ObjectMapper(new JsonFactory());
-        mapper.findAndRegisterModules();
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        JsonMapper.Builder builder = JsonMapper.builder();
+        builder.findAndAddModules();
+        builder.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES);
+        builder.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper = builder.build();
     }
 
     public <T> T deserialize(String serialized, Class<T> cls) throws JsonProcessingException {
